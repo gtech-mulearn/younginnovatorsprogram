abe21a58bcb5a137b38287b275d29648494556b5
Aswin Asok <aswinasokofficial@gmail.com>
2022-11-03 21:35:34 +0530

    Styles Updations and Corrections

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Yip from "./Pages/Landing/Yip";
 
 function App() {
   return (
     <div className="App">
-      
+      <Yip />
     </div>
   );
 }
